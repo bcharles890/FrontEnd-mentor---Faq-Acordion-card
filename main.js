1e44faf221d77fc27bb2d6300124596f6e3b9aa1
brandoncharlesBrockU <bc21yf@brocku.ca>
2023-10-04 01:22:08 -0400

    most html comleted working on the styling of css

---
+++ main.js
@@ -0,0 +1,8 @@
+const accordion1 = document.getElementById('accordion1');
+
+const accordion1Display = document.getElementById('accordion1Display');
+
+accordion1Display.addEventListener(onclick, e =>{
+
+    document.body.style.display = 'none';
+});
