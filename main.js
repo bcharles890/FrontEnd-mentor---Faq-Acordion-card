1d85cc1dded2169b71bb7aed7ba7852310fca69a
brandoncharlesBrockU <bc21yf@brocku.ca>
2023-10-10 03:26:04 -0400

    working on positioning

--- main.js
+++
@@ -1,8 +0,0 @@
-const accordion1 = document.getElementById('accordion1');
-
-const accordion1Display = document.getElementById('accordion1Display');
-
-accordion1Display.addEventListener(onclick, e =>{
-
-    document.body.style.display = 'none';
-});
